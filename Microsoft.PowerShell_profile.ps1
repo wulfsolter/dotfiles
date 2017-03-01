3d015c5faa93ecd90bc13314cd6d7a349444cdf7
Wulf <wulf@wulf.co.nz>
2017-03-02 11:18:22 +1300

    PowerShell profile updates

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -15,28 +15,38 @@ function Docker-Connect {
 }
 
 function Docker-Run {
-	C:\Users\User\code\docker\postgres\run.ps1
-	C:\Users\User\code\docker\dev\run.ps1
+    C:\Users\User\code\docker\postgres\run.ps1
+    C:\Users\User\code\docker\dev\run.ps1
 }
 
 function Docker-Root {
-	docker exec -t -i dev /bin/bash
+    docker exec -t -i dev /bin/bash
 }
 
-function Sublime([string]$arg1) {
-	C:\Program` Files\Sublime` Text` 3\sublime_text.exe $arg1
+function gits {
+    git status
+}
+
+function subl([string]$arg1) {
+    C:\Program` Files\Sublime` Text` 3\sublime_text.exe $arg1
 }
 
 function Meld([string]$arg1, [string]$arg2) {
-	C:\Program` Files` `(x86`)\Meld\Meld.exe $arg1 $arg2
+    C:\Program` Files` `(x86`)\Meld\Meld.exe $arg1 $arg2
 }
 
-function GitStatus {
-	git status
+function reboot() {
+    shutdown -t 0 -r -f
+}
+
+function tig() {
+    if(-Not ($env:PATH.Contains("C:\cygwin64\bin"))) {
+        echo 'cygwin not in path, appending environment variable'
+        $env:PATH += ";C:\cygwin64\bin\";
+    }
+    iex -c 'C:\cygwin64\bin\bash.exe -c "/usr/bin/tig"';
 }
 
 Set-Location ~/code
 Set-Alias dc Docker-Connect
 Set-Alias dr Docker-Run
-Set-Alias subl Sublime
-Set-Alias gits GitStatus
